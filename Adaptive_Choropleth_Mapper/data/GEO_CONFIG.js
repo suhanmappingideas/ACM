2d6efa5c76c53a47e605bb7f621d785bb07f5590
Su Han <suhanmappingideas@gmail.com>
2018-10-01 02:35:52 -0700

    Add files via upload

---
+++ Adaptive_Choropleth_Mapper/data/GEO_CONFIG.js
@@ -0,0 +1,32 @@
+// Define the number of maps that you want to visaulize. Upto 15 maps are supported.
+
+var NumOfMaps = 4; 
+
+/*
+/* Example for defining the variabels that you want to visaulize. */
+/*
+var InitialLayers = ["Heart Disease10", "Heart Disease11", "Heart Disease12", "Heart Disease13"];
+/*
+
+/* Map Extent and Zoom level will be automatically adjusted when you do not define map center and zoom level */
+/* Examples for defining map center and zoom level for San Diego and Los Angeles metro area*/
+/*
+var Initial_map_center = [33.01959536, -116.8387589];      // SD
+var Initial_map_zoom_level = 13;                           // range 1 to 20 ?
+
+var Initial_map_center = [33.786671, -118.182354];         // LA
+var Initial_map_zoom_level = 10;                           // range 1 to 20 ? 
+*/
+
+/* The stacked chart shows the change of number of polygons belonging to each class intervals 
+   The chart appears only when the map extent and the class intervals of all maps are same.
+   To make all maps have the same map extent and class intervals, enable "Grouping All" or click "Sync" on each of maps   */
+var Stacked_Chart = true;                                  // true or false
+
+//var Num_Of_Decimal_Places = 2;                             // default = 1
+
+var Map_width  = "450px";                                  // min 350px
+var Map_height = "450px";                                  // min 250px
+
+var Chart_width  = "450px";                                // min 300px
+var Chart_height = "300px";                                // min 200px
